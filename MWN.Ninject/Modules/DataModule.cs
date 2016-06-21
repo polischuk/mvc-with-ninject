e214a2f6149c91d57d6c8e98bf31d7c2dfdd2663
Artem Polischuk <polischukao@gmail.com>
2016-06-21 16:40:39 +0300

    Added missed files

---
+++ MWN.Ninject/Modules/DataModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security.DataProtection;
+using MWN.Data;
+using MWN.Data.Entities;
+using MWN.Data.Managers;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace MWN.Ninject.Modules
+{
+    public class DataModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ApplicationUserManager>()
+                .ToMethod(
+                    context =>
+                        new ApplicationUserManager(
+                            new UserStore<ApplicationUser>(
+                                Kernel.GetService(typeof (ApplicationDbContext)) as ApplicationDbContext)))
+                .InRequestScope();
+            var ifo = new IdentityFactoryOptions<ApplicationSignInManager>
+            {
+                DataProtectionProvider = DependencyResolver.Current.GetService<IDataProtectionProvider>()
+            };
+            Bind<ApplicationSignInManager>().ToMethod(m => ApplicationSignInManager.Create(ifo, HttpContext.Current.GetOwinContext())).InRequestScope();
+        }
+    }
+}
---
+++ MWN.Ninject/StartupConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MWN.Ninject
+{
+    /// <summary>
+    /// The startup config.
+    /// </summary>
+    public class StartupConfig
+    {
+        private static System.Web.Http.HttpConfiguration _config;
+
+        /// <summary>
+        /// Gets the config.
+        /// </summary>
+        public static HttpConfiguration Config => _config ?? (_config = new HttpConfiguration());
+    }
+}
